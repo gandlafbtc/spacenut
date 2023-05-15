7a41515e865a49c71894163b0b5057b0a5c7112b
gandlaf21 <gandlaf@proton.me>
2023-05-15 18:42:45 +0200

    no scrollbars

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -11,7 +11,7 @@
 <div class="w-screen h-screen bg-black flex flex-col items-center gap-5">
 	<div id="app" class="h-[500px]" />
 	<a href="/stash" class="link link-primary">nutstash</a>
-	<div class="p-2 border-white border-2 w-64 h-40 rounded-xl overflow-scroll">
+	<div class="p-2 border-white border-2 w-64 h-40 rounded-xl overflow-scroll scrollbar-hide">
 		<ul id="coins-list" style="color: aliceblue;" />
 	</div>
 </div>
